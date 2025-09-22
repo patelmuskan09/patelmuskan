1751cf1a9ef965f5ca5f8c77ad7f3a732620904a
patelmuskan09 <patelmuskan672@gmail.com>
2025-09-22 13:46:18 +0530

    Create Uright.c

---
+++ Uright.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    for(int i=0;i<3;i++){
+        for(int k=0;k<=i;k++){
+            printf(" ");
+        }
+        for(int j=3;j>i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
