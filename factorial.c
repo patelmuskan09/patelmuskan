30041a994977a7e9372a7493d1231468dee26ec2
patelmuskan09 <patelmuskan672@gmail.com>
2025-10-30 13:33:49 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fact(int a){ //a is parameter
+   // return a;
+   if(a==0 || a==1){
+    return 1;
+   }
+  return a*fact(a-1);
+}
+int main(){
+  int data= fact(5);
+  printf("%d",data);
+    
+}
