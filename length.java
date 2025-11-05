acaa48d73a671f147d069d0fe3dc7f9e20c93218
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-05 12:47:21 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,9 @@
+
+public class length {
+    public static void main(String[] args) {
+        String str="hello";
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
