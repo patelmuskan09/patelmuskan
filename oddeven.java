09329e479d682cc323d934058feebcf7211da145
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-05 12:47:50 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+
+   public class oddeven {
+       public static void main(String[] args) {
+
+           int a = 6;
+           if (a % 2 == 0) {
+               System.out.println("EVEN");
+           } else {
+               System.out.println("ODD");
+           }
+       }
+   }
