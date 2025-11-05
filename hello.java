433d63cc1c8eb322151ee320fbf3cc0a759f0dae
patelmuskan09 <patelmuskan672@gmail.com>
2025-11-05 12:46:50 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+
+
+        public static void main(String[] args) {
+            byte a = 5;
+            short b = 15;
+            int c = 20;
+            float f = 90;
+            boolean data = false;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(f);
+        }
+    }
+
