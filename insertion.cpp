f7cbb4e5bfac3777f35f2e729a4ecc2c2d37efd3
patelmuskan09 <patelmuskan672@gmail.com>
2026-06-06 23:05:57 +0530

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+//TRAVERSAL
+void display(int arr[], int n){
+    for(int i=0; i<n; i++ ){
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<endl;
+}
+//INSERTION AT AN INDEX
+int indinsertion(int arr[] , int size, int element ,int capacity, int index){
+   if(size>=capacity){
+    return -1;
+   }
+   for(int i=size; i>=index; i--){
+        arr[i+1]=arr[i];
+   }
+   arr[index]=element;
+ return 1;  
+}
+
+int main(){
+    int arr[100]={7,8,12,27,88};
+    int size=5, element=45,index=3;
+    display(arr, size);
+    indinsertion(arr , size, element ,100, index);
+    size=size+1;
+    display(arr,size);
+return 0;
+}
